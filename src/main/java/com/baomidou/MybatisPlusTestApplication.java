03fb0df9347998d5f839d68b2d1e66bd746ba568
chenkaiyu <kaiyu.chen@chintanneng.com>
2025-05-30 17:24:44 +0800

    简化

--- src/main/java/com/baomidou/MybatisPlusTestApplication.java
+++ src/main/java/com/baomidou/MybatisPlusTestApplication.java
@@ -1,15 +1,28 @@
 package com.baomidou;
 
+import com.alibaba.cloud.nacos.NacosConfigAutoConfiguration;
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.cloud.nacos.endpoint.NacosConfigEndpointAutoConfiguration;
+import com.alibaba.nacos.api.config.ConfigService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
 @MapperScan("com.baomidou.ant.blog.mapper")
+@SpringBootApplication //
+// 先排除自动配置
+
 public class MybatisPlusTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MybatisPlusTestApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(MybatisPlusTestApplication.class, args);
+
+
+        // 手动触发 Nacos 配置加载
+        NacosConfigManager nacosConfigManager = context.getBean(NacosConfigManager.class);
+        ConfigService configService = nacosConfigManager.getConfigService();
+        String serverStatus = configService.getServerStatus();
     }
 
 }
