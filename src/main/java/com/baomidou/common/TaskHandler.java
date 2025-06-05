56e415dea1b259c6b81a67755fefc1638418c96b
chenkaiyu <kaiyu.chen@chintanneng.com>
2025-06-05 14:20:37 +0800

    简化

--- src/main/java/com/baomidou/common/TaskHandler.java
+++ src/main/java/com/baomidou/common/TaskHandler.java
@@ -12,8 +12,17 @@ import com.xxl.job.core.log.XxlJobFileAppender;
 public class TaskHandler {
 
     @XxlJob("test01Handler")
-    public  void test01Handler() {
-        XxlJobHelper.log("xxl-job, hello world.");
-        log.info("xxl-job, hello world.");
+    public  void test01Handler() throws InterruptedException {
+      //  XxlJobHelper.log("xxl-job, hello world.");
+        int shardIndex = XxlJobHelper.getShardIndex();
+        int shardTotal = XxlJobHelper.getShardTotal();
+        log.info("xxl-job, shardIndex: {}, shardTotal: {}", shardIndex, shardTotal);
+        try {
+            Thread.sleep(10000);
+        }catch (Exception e){
+            log.info("xxl-job, 被中止啦");
+        }
+
+        log.info("xxl-job, end");
     }
 }
